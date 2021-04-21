c47d70d2b735224de37a2f07c1d6628745c7f81b
xenon-92 <sumitjana92@hotmail.com>
2021-04-22 02:08:22 +0530

    changes

---
+++ TreeDs/TreeDS_Prac1/12_CheckIfBTisBalancedOptimal/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _12_CheckIfBTisBalancedOptimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val)
+        {
+            this.val = val;
+        }
+    }
+
+    class Solution
+    {
+        public bool IsBalanced(TreeNode root)
+        {
+            if (UtilityBalanced(root) == -1) return false;
+            return true;
+        }
+        int UtilityBalanced(TreeNode root)
+        {
+            if (root == null) return 0;
+            int l = UtilityBalanced(root.left);
+            if (l == -1) return -1;
+            int r = UtilityBalanced(root.right);
+            if (r == -1) return -1;
+            if (Math.Abs(l - r) > 1)
+            {
+                return -1;
+            }
+            else
+            {
+                return Math.Max(l, r) + 1;
+            }
+        }
+    }
+}
---
+++ TreeDs/TreeDS_Prac1/13_TreeTraversalInSpiral/Program.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _13_TreeTraversalInSpiral
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            s.PrintSpiralOrder(TestSolution.DesignBT3());
+        }
+    }
+
+    class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val)
+        {
+            this.val = val;
+        }
+    }
+    class Solution
+    {
+        public void PrintSpiralOrder(TreeNode root)
+        {
+            if (root == null) return;
+            Queue<TreeNode> q = new Queue<TreeNode>();
+            q.Enqueue(root);
+            bool isReversed = false;
+            while (q.Count != 0)
+            {
+                var c = q.Count;
+                int[] temp = new int[c];
+                for (int i = 0; i < c; i++)
+                {
+                    var d = q.Dequeue();
+                    temp[i] = d.val;
+                    if (d.left != null)
+                    {
+                        q.Enqueue(d.left);
+                    }
+                    if (d.right != null)
+                    {
+                        q.Enqueue(d.right);
+                    }
+                }
+
+                if (isReversed)
+                {
+                    int i = temp.Length - 1;
+                    while (i >= 0)
+                    {
+                        System.Console.Write(temp[i] + " ");
+                        i -= 1;
+                    }
+                    isReversed = !isReversed;
+                    System.Console.WriteLine();
+                }
+                else
+                {
+                    int i = 0;
+                    while (i < temp.Length)
+                    {
+                        System.Console.Write(temp[i] + " ");
+                        i += 1;
+                    }
+                    isReversed = !isReversed;
+                    System.Console.WriteLine();
+                }
+            }
+        }
+    }
+    class TestSolution
+    {
+        public static TreeNode DesignBT1()
+        {
+            //         1                                                   
+            //        / \                                                
+            //       2   3                                               
+            //      /\   /\                                                
+            //     4  5  6 7                                               
+            //              \                                            
+            //               8
+            //               /\                            
+            //              9  10
+
+            TreeNode node1 = new TreeNode(1);
+
+            TreeNode node2 = new TreeNode(2);
+            TreeNode node3 = new TreeNode(3);
+            node1.left = node2;
+            node1.right = node3;
+
+            TreeNode node4 = new TreeNode(4);
+            TreeNode node5 = new TreeNode(5);
+            node2.left = node4;
+            node2.right = node5;
+
+            TreeNode node6 = new TreeNode(6);
+            TreeNode node7 = new TreeNode(7);
+            node3.left = node6;
+            node3.right = node7;
+
+            TreeNode node8 = new TreeNode(8);
+            node7.right = node8;
+
+            TreeNode node9 = new TreeNode(9);
+            TreeNode node10 = new TreeNode(10);
+            node8.left = node9;
+            node8.right = node10;
+            return node1;
+        }
+        public static TreeNode DesignBT2()
+        {
+
+            //         1                                                   
+            //        / \                                                
+            //       2   3                                               
+            //      /\   /\                                                
+            //     4  5  6 7                                               
+            //    /\       / \                                            
+            //   8  9      10 11    
+            TreeNode node1 = new TreeNode(1);
+
+            TreeNode node2 = new TreeNode(2);
+            TreeNode node3 = new TreeNode(3);
+            node1.left = node2;
+            node1.right = node3;
+
+            TreeNode node4 = new TreeNode(4);
+            TreeNode node5 = new TreeNode(5);
+            node2.left = node4;
+            node2.right = node5;
+
+            TreeNode node6 = new TreeNode(6);
+            TreeNode node7 = new TreeNode(7);
+            node3.left = node6;
+            node3.right = node7;
+            TreeNode node8 = new TreeNode(8);
+            TreeNode node9 = new TreeNode(9);
+            node4.left = node8;
+            node4.right = node9;
+            TreeNode node10 = new TreeNode(10);
+            TreeNode node11 = new TreeNode(11);
+            node7.left = node10;
+            node7.right = node11;
+            Console.WriteLine("Full BT");
+            return node1;
+        }
+        public static TreeNode DesignBT3()
+        {
+
+            //         1                                                   
+            //        / \                                                
+            //       2   3                                               
+            //      /\   /\                                                
+            //     4  5  6 null                                              
+
+            TreeNode node1 = new TreeNode(1);
+
+            TreeNode node2 = new TreeNode(2);
+            TreeNode node3 = new TreeNode(3);
+            node1.left = node2;
+            node1.right = node3;
+
+            TreeNode node4 = new TreeNode(4);
+            TreeNode node5 = new TreeNode(5);
+            node2.left = node4;
+            node2.right = node5;
+
+            TreeNode node6 = new TreeNode(6);
+            Console.WriteLine("Complete BT");
+            node3.left = node6;
+            return node1;
+        }
+        public static TreeNode DesignBT4()
+        {
+
+            //         1                                                   
+            //        / \                                                
+            //       2   3                                               
+            //      /\   /\                                                
+            //     4  5  6 7                                               
+
+            TreeNode node1 = new TreeNode(1);
+
+            TreeNode node2 = new TreeNode(2);
+            TreeNode node3 = new TreeNode(3);
+            node1.left = node2;
+            node1.right = node3;
+
+            TreeNode node4 = new TreeNode(4);
+            TreeNode node5 = new TreeNode(5);
+            node2.left = node4;
+            node2.right = node5;
+            Console.WriteLine("Perfect BT");
+            TreeNode node6 = new TreeNode(6);
+            TreeNode node7 = new TreeNode(7);
+            node3.left = node6;
+            node3.right = node7;
+            return node1;
+        }
+        public static TreeNode DesignBT5()
+        {
+
+            //         1                                                   
+            //        / \                                                
+            //       2   3                                               
+            //      /                                             
+            //     4                                             
+
+            TreeNode node1 = new TreeNode(1);
+
+            TreeNode node2 = new TreeNode(2);
+            TreeNode node3 = new TreeNode(3);
+            node1.left = node2;
+            node1.right = node3;
+
+            TreeNode node4 = new TreeNode(4);
+
+            node2.left = node4;
+            Console.WriteLine("Balanced binary tree");
+            return node1;
+        }
+        public static TreeNode DesignBT6()
+        {
+
+            //         1                                                   
+            //        /                                              
+            //       2                                               
+            //       \                                                 
+            //        3
+            //       /
+            //      4
+
+            TreeNode node1 = new TreeNode(1);
+
+            TreeNode node2 = new TreeNode(2);
+            TreeNode node3 = new TreeNode(3);
+            node1.left = node2;
+            node2.right = node3;
+            TreeNode node4 = new TreeNode(4);
+            node3.left = node4;
+            Console.WriteLine("Degenerate BT");
+            return node1;
+        }
+        public static TreeNode DesignBT7()
+        {
+
+            //         1                                                   
+            //        /                                              
+            //       2                                               
+            //      /                                                 
+            //     3
+            //    /
+            //   4
+
+            TreeNode node1 = new TreeNode(1);
+
+            TreeNode node2 = new TreeNode(2);
+            TreeNode node3 = new TreeNode(3);
+            node1.left = node2;
+            node2.left = node3;
+            TreeNode node4 = new TreeNode(4);
+            node3.left = node4;
+            return node1;
+        }
+
+        public static TreeNode DesignBT8()
+        {
+
+            //         1                                                   
+            //          \                                              
+            //           2                                               
+            //            \                                                 
+            //             3
+            //              \
+            //               4
+
+            TreeNode node1 = new TreeNode(1);
+
+            TreeNode node2 = new TreeNode(2);
+            TreeNode node3 = new TreeNode(3);
+            node1.right = node2;
+            node2.right = node3;
+            TreeNode node4 = new TreeNode(4);
+            node3.right = node4;
+            return node1;
+        }
+        public static TreeNode LC662_Test()
+        {
+
+            //         1                                                   
+            //        / \                                                
+            //       2   3                                               
+            //      /\   /\                                                
+            //     4       7                                               
+            //    /\       / \                                            
+            //   8           11    
+            TreeNode node1 = new TreeNode(1);
+
+            TreeNode node2 = new TreeNode(2);
+            TreeNode node3 = new TreeNode(3);
+            node1.left = node2;
+            node1.right = node3;
+
+            TreeNode node4 = new TreeNode(4);
+            node2.left = node4;
+
+            TreeNode node7 = new TreeNode(7);
+            node3.right = node7;
+            TreeNode node8 = new TreeNode(8);
+            node4.left = node8;
+            TreeNode node11 = new TreeNode(11);
+            node7.right = node11;
+            Console.WriteLine("Full BT");
+            return node1;
+        }
+        public static TreeNode LC662_Test2()
+        {
+
+            //         1                                                   
+            //        / \                                                
+            //       2   3                                               
+            //      /\   /\                                                
+            //     4       7                                               
+            //    /\       / \                                            
+            //   8        10    
+            TreeNode node1 = new TreeNode(1);
+
+            TreeNode node2 = new TreeNode(2);
+            TreeNode node3 = new TreeNode(3);
+            node1.left = node2;
+            node1.right = node3;
+
+            TreeNode node4 = new TreeNode(4);
+            node2.left = node4;
+
+            TreeNode node7 = new TreeNode(7);
+            node3.right = node7;
+            TreeNode node8 = new TreeNode(8);
+            node4.left = node8;
+            TreeNode node10 = new TreeNode(10);
+            node7.left = node10;
+            Console.WriteLine("Full BT");
+            return node1;
+        }
+    }
+}
