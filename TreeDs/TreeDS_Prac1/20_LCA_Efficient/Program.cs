77078afff57bfc906e22cc13703f3508cb96b1b0
xenon-92 <sumitjana92@hotmail.com>
2021-04-27 22:49:23 +0530

    LCA efficient

---
+++ TreeDs/TreeDS_Prac1/20_LCA_Efficient/Program.cs
@@ -0,0 +1,345 @@
+﻿using System;
+
+namespace _20_LCA_Efficient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            var res = s.LCA(TestSolution.DesignBT1(), 4, 5);
+            System.Console.WriteLine(res);
+        }
+    }
+
+    class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val)
+        {
+            this.val = val;
+        }
+    }
+
+    // This solution assumes that both the nodes are present in the tree.
+    // if any of node is missing in the tree, then the result is incorrect
+    class Solution
+    {
+        public int LCA(TreeNode root, int n1, int n2)
+        {
+            return Utility(root, n1, n2);
+        }
+
+        int Utility(TreeNode root, int n1, int n2)
+        {
+            if (root == null) return -1;
+
+            if (root.val == n1 || root.val == n2)
+            {
+                return root.val;
+            }
+
+            int left = Utility(root.left, n1, n2);
+            int right = Utility(root.right, n1, n2);
+
+            if (left != -1 && right != -1)
+            {
+                System.Console.WriteLine($"Both conditions met {root.val}");
+                return root.val;
+            }
+            else
+            {
+                if (left != -1)
+                {
+                    System.Console.WriteLine($"left returned {left}");
+                    return left;
+                }
+                else if (right != -1)
+                {
+                    System.Console.WriteLine($"right returned {right}");
+                    return right;
+                }
+                System.Console.WriteLine("Neither left !=-1 or right !=-1");
+                return -1;
+            }
+        }
+    }
+    class TestSolution
+    {
+        public static TreeNode DesignBT1()
+        {
+            //         1                                                   
+            //        / \                                                
+            //       2   3                                               
+            //      /\   /\                                                
+            //     4  5  6 7                                               
+            //              \                                            
+            //               8
+            //               /\                            
+            //              9  10
+
+            TreeNode node1 = new TreeNode(1);
+
+            TreeNode node2 = new TreeNode(2);
+            TreeNode node3 = new TreeNode(3);
+            node1.left = node2;
+            node1.right = node3;
+
+            TreeNode node4 = new TreeNode(4);
+            TreeNode node5 = new TreeNode(5);
+            node2.left = node4;
+            node2.right = node5;
+
+            TreeNode node6 = new TreeNode(6);
+            TreeNode node7 = new TreeNode(7);
+            node3.left = node6;
+            node3.right = node7;
+
+            TreeNode node8 = new TreeNode(8);
+            node7.right = node8;
+
+            TreeNode node9 = new TreeNode(9);
+            TreeNode node10 = new TreeNode(10);
+            node8.left = node9;
+            node8.right = node10;
+            return node1;
+        }
+        public static TreeNode DesignBT2()
+        {
+
+            //         1                                                   
+            //        / \                                                
+            //       2   3                                               
+            //      /\   /\                                                
+            //     4  5  6 7                                               
+            //    /\       / \                                            
+            //   8  9      10 11    
+            TreeNode node1 = new TreeNode(1);
+
+            TreeNode node2 = new TreeNode(2);
+            TreeNode node3 = new TreeNode(3);
+            node1.left = node2;
+            node1.right = node3;
+
+            TreeNode node4 = new TreeNode(4);
+            TreeNode node5 = new TreeNode(5);
+            node2.left = node4;
+            node2.right = node5;
+
+            TreeNode node6 = new TreeNode(6);
+            TreeNode node7 = new TreeNode(7);
+            node3.left = node6;
+            node3.right = node7;
+            TreeNode node8 = new TreeNode(8);
+            TreeNode node9 = new TreeNode(9);
+            node4.left = node8;
+            node4.right = node9;
+            TreeNode node10 = new TreeNode(10);
+            TreeNode node11 = new TreeNode(11);
+            node7.left = node10;
+            node7.right = node11;
+            Console.WriteLine("Full BT");
+            return node1;
+        }
+        public static TreeNode DesignBT3()
+        {
+
+            //         1                                                   
+            //        / \                                                
+            //       2   3                                               
+            //      /\   /\                                                
+            //     4  5  6 null                                              
+
+            TreeNode node1 = new TreeNode(1);
+
+            TreeNode node2 = new TreeNode(2);
+            TreeNode node3 = new TreeNode(3);
+            node1.left = node2;
+            node1.right = node3;
+
+            TreeNode node4 = new TreeNode(4);
+            TreeNode node5 = new TreeNode(5);
+            node2.left = node4;
+            node2.right = node5;
+
+            TreeNode node6 = new TreeNode(6);
+            Console.WriteLine("Complete BT");
+            node3.left = node6;
+            return node1;
+        }
+        public static TreeNode DesignBT4()
+        {
+
+            //         1                                                   
+            //        / \                                                
+            //       2   3                                               
+            //      /\   /\                                                
+            //     4  5  6 7                                               
+
+            TreeNode node1 = new TreeNode(1);
+
+            TreeNode node2 = new TreeNode(2);
+            TreeNode node3 = new TreeNode(3);
+            node1.left = node2;
+            node1.right = node3;
+
+            TreeNode node4 = new TreeNode(4);
+            TreeNode node5 = new TreeNode(5);
+            node2.left = node4;
+            node2.right = node5;
+            Console.WriteLine("Perfect BT");
+            TreeNode node6 = new TreeNode(6);
+            TreeNode node7 = new TreeNode(7);
+            node3.left = node6;
+            node3.right = node7;
+            return node1;
+        }
+        public static TreeNode DesignBT5()
+        {
+
+            //         1                                                   
+            //        / \                                                
+            //       2   3                                               
+            //      /                                             
+            //     4                                             
+
+            TreeNode node1 = new TreeNode(1);
+
+            TreeNode node2 = new TreeNode(2);
+            TreeNode node3 = new TreeNode(3);
+            node1.left = node2;
+            node1.right = node3;
+
+            TreeNode node4 = new TreeNode(4);
+
+            node2.left = node4;
+            Console.WriteLine("Balanced binary tree");
+            return node1;
+        }
+        public static TreeNode DesignBT6()
+        {
+
+            //         1                                                   
+            //        /                                              
+            //       2                                               
+            //       \                                                 
+            //        3
+            //       /
+            //      4
+
+            TreeNode node1 = new TreeNode(1);
+
+            TreeNode node2 = new TreeNode(2);
+            TreeNode node3 = new TreeNode(3);
+            node1.left = node2;
+            node2.right = node3;
+            TreeNode node4 = new TreeNode(4);
+            node3.left = node4;
+            Console.WriteLine("Degenerate BT");
+            return node1;
+        }
+        public static TreeNode DesignBT7()
+        {
+
+            //         1                                                   
+            //        /                                              
+            //       2                                               
+            //      /                                                 
+            //     3
+            //    /
+            //   4
+
+            TreeNode node1 = new TreeNode(1);
+
+            TreeNode node2 = new TreeNode(2);
+            TreeNode node3 = new TreeNode(3);
+            node1.left = node2;
+            node2.left = node3;
+            TreeNode node4 = new TreeNode(4);
+            node3.left = node4;
+            return node1;
+        }
+
+        public static TreeNode DesignBT8()
+        {
+
+            //         1                                                   
+            //          \                                              
+            //           2                                               
+            //            \                                                 
+            //             3
+            //              \
+            //               4
+
+            TreeNode node1 = new TreeNode(1);
+
+            TreeNode node2 = new TreeNode(2);
+            TreeNode node3 = new TreeNode(3);
+            node1.right = node2;
+            node2.right = node3;
+            TreeNode node4 = new TreeNode(4);
+            node3.right = node4;
+            return node1;
+        }
+        public static TreeNode LC662_Test()
+        {
+
+            //         1                                                   
+            //        / \                                                
+            //       2   3                                               
+            //      /\   /\                                                
+            //     4       7                                               
+            //    /\       / \                                            
+            //   8           11    
+            TreeNode node1 = new TreeNode(1);
+
+            TreeNode node2 = new TreeNode(2);
+            TreeNode node3 = new TreeNode(3);
+            node1.left = node2;
+            node1.right = node3;
+
+            TreeNode node4 = new TreeNode(4);
+            node2.left = node4;
+
+            TreeNode node7 = new TreeNode(7);
+            node3.right = node7;
+            TreeNode node8 = new TreeNode(8);
+            node4.left = node8;
+            TreeNode node11 = new TreeNode(11);
+            node7.right = node11;
+            Console.WriteLine("Full BT");
+            return node1;
+        }
+        public static TreeNode LC662_Test2()
+        {
+
+            //         1                                                   
+            //        / \                                                
+            //       2   3                                               
+            //      /\   /\                                                
+            //     4       7                                               
+            //    /\       / \                                            
+            //   8        10    
+            TreeNode node1 = new TreeNode(1);
+
+            TreeNode node2 = new TreeNode(2);
+            TreeNode node3 = new TreeNode(3);
+            node1.left = node2;
+            node1.right = node3;
+
+            TreeNode node4 = new TreeNode(4);
+            node2.left = node4;
+
+            TreeNode node7 = new TreeNode(7);
+            node3.right = node7;
+            TreeNode node8 = new TreeNode(8);
+            node4.left = node8;
+            TreeNode node10 = new TreeNode(10);
+            node7.left = node10;
+            Console.WriteLine("Full BT");
+            return node1;
+        }
+    }
+}
