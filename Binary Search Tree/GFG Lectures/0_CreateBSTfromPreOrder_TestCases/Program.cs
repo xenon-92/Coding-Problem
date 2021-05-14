c638388c008aa6bc7ce4a8d2dd774e66fddd493f
xenon-92 <sumitjana92@hotmail.com>
2021-05-14 23:20:28 +0530

    L0

---
+++ Binary Search Tree/GFG Lectures/0_CreateBSTfromPreOrder_TestCases/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _0_CreateBSTfromPreOrder_TestCases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val)
+        {
+            this.val = val;
+        }
+    }
+
+    class TestSolution
+    {
+        public static TreeNode CreateBST1()
+        {
+            return null;
+        }
+    }
+}
