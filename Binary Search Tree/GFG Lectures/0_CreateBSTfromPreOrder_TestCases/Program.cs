8769edad9fcf12bd0a45fb601583f28a8132c206
xenon-92 <sumitjana92@hotmail.com>
2021-05-15 00:36:41 +0530

    p0

--- Binary Search Tree/GFG Lectures/0_CreateBSTfromPreOrder_TestCases/Program.cs
+++ Binary Search Tree/GFG Lectures/0_CreateBSTfromPreOrder_TestCases/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _0_CreateBSTfromPreOrder_TestCases
 {
@@ -6,7 +7,27 @@ namespace _0_CreateBSTfromPreOrder_TestCases
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var res = TestSolution.DesignBST8();
+            PrintBFS(res);
+        }
+
+        static void PrintBFS(TreeNode root)
+        {
+            if (root == null) return;
+            Queue<TreeNode> q = new Queue<TreeNode>();
+            q.Enqueue(root);
+            while (q.Count != 0)
+            {
+                var c = q.Count;
+                for (int i = 0; i < c; i++)
+                {
+                    var d = q.Dequeue();
+                    System.Console.Write(d.val + " ");
+                    if (d.left != null) q.Enqueue(d.left);
+                    if (d.right != null) q.Enqueue(d.right);
+                }
+                System.Console.WriteLine();
+            }
         }
     }
 
@@ -23,9 +44,80 @@ namespace _0_CreateBSTfromPreOrder_TestCases
 
     class TestSolution
     {
-        public static TreeNode CreateBST1()
+        static int idx = 0;
+
+        /// Creates a BST from preorder traversal of BST
+
+        static TreeNode UtilityCreateBST(int[] pre, int minval, int maxval)
         {
-            return null;
+            if (idx > pre.Length - 1) return null;
+            int nowVal = pre[idx];
+            if (nowVal >= maxval || nowVal <= minval) return null;
+            TreeNode root = new TreeNode(nowVal);
+            idx += 1;
+            root.left = UtilityCreateBST(pre, minval, nowVal);
+            root.right = UtilityCreateBST(pre, nowVal, maxval);
+            return root;
+        }
+        public static TreeNode DesignBST1()
+        {
+            int[] preOrder = { };
+            if (preOrder == null || preOrder.Length == 0) return null;
+            idx = 0;
+            return UtilityCreateBST(preOrder, int.MinValue, int.MaxValue);
+        }
+        public static TreeNode DesignBST2()
+        {
+            int[] preOrder = { 8, 4, 2, 1, 3, 6, 5, 7, 12, 10, 9, 11, 14, 13, 15 };
+            if (preOrder == null || preOrder.Length == 0) return null;
+            idx = 0;
+            return UtilityCreateBST(preOrder, int.MinValue, int.MaxValue);
+        }
+        public static TreeNode DesignBST3()
+        {
+            int[] preOrder = { 2, 1, 4, 3, 6, 5, 8, 7, 10, 9, 11 };
+            if (preOrder == null || preOrder.Length == 0) return null;
+            idx = 0;
+            return UtilityCreateBST(preOrder, int.MinValue, int.MaxValue);
+        }
+
+        public static TreeNode DesignBST4()
+        {
+            int[] preOrder = { 8, 4, 2, 1, 3, 6, 5, 7, 11, 10, 9, 12, 13 };
+            if (preOrder == null || preOrder.Length == 0) return null;
+            idx = 0;
+            return UtilityCreateBST(preOrder, int.MinValue, int.MaxValue);
+        }
+
+        public static TreeNode DesignBST5()
+        {
+            int[] preOrder = { 5, 4, 3, 2, 1 };
+            if (preOrder == null || preOrder.Length == 0) return null;
+            idx = 0;
+            return UtilityCreateBST(preOrder, int.MinValue, int.MaxValue);
+        }
+
+        public static TreeNode DesignBST6()
+        {
+            int[] preOrder = { 1, 2, 3, 4, 5 };
+            if (preOrder == null || preOrder.Length == 0) return null;
+            idx = 0;
+            return UtilityCreateBST(preOrder, int.MinValue, int.MaxValue);
+        }
+        public static TreeNode DesignBST7()
+        {
+            int[] preOrder = { 8, 2, 7, 3, 6, 4, 5 };
+            if (preOrder == null || preOrder.Length == 0) return null;
+            idx = 0;
+            return UtilityCreateBST(preOrder, int.MinValue, int.MaxValue);
+        }
+
+        public static TreeNode DesignBST8()
+        {
+            int[] preOrder = { 4, 3, 2, 1, 5, 6, 7 };
+            if (preOrder == null || preOrder.Length == 0) return null;
+            idx = 0;
+            return UtilityCreateBST(preOrder, int.MinValue, int.MaxValue);
         }
     }
 }
