045c4508509f77d3a935e15222dda7f234a6e8c4
xenon-92 <sumitjana92@hotmail.com>
2021-06-13 21:52:07 +0530

    checkifkthBit isset

---
+++ Bit Magic/Lecture/L1_Check_if_kth_bit_is_set_or_not/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace L1_Check_if_kth_bit_is_set_or_not
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            var res = s.CheckIfKthBitIsSet(7, 2);
+            var res1 = s.CheckIfKthBitIsSet_1(13, 2);
+            System.Console.WriteLine(res + " " + res1);
+        }
+    }
+
+    class Solution
+    {
+        //Left shift way
+        public bool CheckIfKthBitIsSet(int n, int k)
+        {
+            if (((1 << k - 1) & n) != 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        //right shift
+        public bool CheckIfKthBitIsSet_1(int n, int k)
+        {
+            if (((n >> k - 1) & 1) != 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
