f7cf9607cd6deb97a9250c19aa4514f750abae0c
xenon-92 <sumitjana92@hotmail.com>
2021-06-14 01:28:13 +0530

    l4,l5

---
+++ Bit Magic/Lecture/L4_One_odd__occuring/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace L4_One_odd__occuring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            int[] arr = { 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 7, 7 };
+            var res = s.OneOddOccuring(arr);
+            //System.Console.WriteLine(res);
+
+            Solution1 s1 = new Solution1();
+            int[] arr1 = { 1, 2, 4 };
+            var res1 = s1.GetMissingNumber(arr1);
+            System.Console.WriteLine(res1);
+        }
+    }
+
+    class Solution
+    {
+        public int OneOddOccuring(int[] arr)
+        {
+            int res = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                res = res ^ arr[i];
+            }
+            return res;
+        }
+    }
+    // Other way of framing it
+    // Given a array of n numbers, that has a value in Range [1,n+1].
+    // Every number appears once. Hence one number is missing, find the missing number
+
+    class Solution1
+    {
+        public int GetMissingNumber(int[] arr)
+        {
+            int res = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                res = res ^ arr[i];
+            }
+            for (int i = 1; i <= arr.Length + 1; i++)
+            {
+                res = res ^ i;
+            }
+            return res;
+        }
+    }
+}
---
+++ Bit Magic/Lecture/L5_Two_odd_occuring/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace L5_Two_odd_occuring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 1, 2, 2, 2, 2, 2, 3, 4, 4, 4, 4, 4, 4, 5, 5 };
+            Solution s = new Solution();
+            s.TwoOddOccuring(arr);
+        }
+    }
+
+    class Solution
+    {
+        public void TwoOddOccuring(int[] arr)
+        {
+            int xor = 0; int res = 0; int res1 = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                xor = xor ^ arr[i];
+            }
+            int rightMostSetBit = xor & ~(xor - 1);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if ((arr[i] & rightMostSetBit) != 0)
+                {
+                    res = res ^ arr[i];
+                }
+                else
+                {
+                    res1 = res1 ^ arr[i];
+                }
+            }
+
+            System.Console.WriteLine(res + " " + res1);
+        }
+    }
+}
