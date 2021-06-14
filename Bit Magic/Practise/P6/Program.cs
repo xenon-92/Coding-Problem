6785e6fc27e4eadbf240839e608e42a850950d42
xenon-92 <sumitjana92@hotmail.com>
2021-06-15 01:18:09 +0530

    P6

---
+++ Bit Magic/Practise/P6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace P6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            s.PrintPowerset("sumit");
+        }
+    }
+
+    class Solution
+    {
+        public void PrintPowerset(string str)
+        {
+            int n = str.Length;
+            int pow_num = (int)Math.Pow(2, n);
+            for (int i = 0; i < pow_num; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if ((i & (1 << j)) != 0)
+                    {
+                        System.Console.Write(str[j]);
+                    }
+                }
+                System.Console.WriteLine();
+            }
+        }
+    }
+}
